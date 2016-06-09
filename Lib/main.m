16627008c576e04ba9961f00de7d12215e1a825a
Joshua Fagan <jfagan2@vols.utk.edu>
2016-06-09 17:24:50 -0400

    Load data from specified files
    Load testing, training, validation data
    Check to ensure number of features and label components are the same
        across all data sets
    Check to ensure number of features and label components match supplied
        neural network architecture

--- Lib/main.m
+++ Lib/main.m
@@ -8,32 +8,54 @@
 % Parameter: opts - Structure storing user specified hyperparameter values
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [] = main( opts )
-    % Check for proper opts content
-    quitting = 0;
+    %%%%% Check for proper opts content
+    returning = 0;
 
     if isfield( opts, 'arch' ) == 0
         disp( 'No architecture specified' )
-        quitting = 1;
+        returning = 1;
     elseif isfield( opts, 'test' ) == 0
         disp( 'No testing file specified' )
-        quitting = 1;
+        returning = 1;
     elseif isfield( opts, 'train' ) == 0
         disp( 'No training file specified' )
-        quitting = 1;
+        returning = 1;
     elseif isfield( opts, 'val' ) == 0
         disp( 'No validation file specified' )
-        quitting = 1;
+        returning = 1;
     end
 
-    if quitting
-        quit
+    if returning
+       return 
     end
 
-    disp( 'hello world' )
+    %%%%% Load data from specified files
+    Test  = load( opts.test );
+    Train = load( opts.train );
+    Val   = load( opts.val );
+    
+    % Check to ensure size of point label and feature information is the same
+    % across data sets
+    if size(Test, 2) ~= size(Train, 2)
+        disp( 'Test data and train data do not have the same number of ' )
+        disp( 'features or label information' )
+        return 
+    elseif size(Test, 2) ~= size(Val, 2)
+        disp( 'Test data and validation data do not have the same number of ' )
+        disp( 'features or label information' )
+       return 
+    end
 
-    % Load data from specified files
+    % Check to ensure size of point label and feature information is the same 
+    % as the sum of the specified inout and output layer size
+    if size(Test,2) ~= ( opts.arch(1) + opts.arch(end) )
+        disp( 'Inappropriate architecture settings:' ) 
+        disp( 'Input layer and output layer should sum to size of data rows' )
+       return 
+    end
 
-    % Create weight and layer matrices
 
-    % Launch backpropagation neural network algorithm
+    %%%%% Create weight and layer matrices
+
+    %%%%% Launch backpropagation neural network algorithm
 end
